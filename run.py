4316e717df891768407d84852792dc969daf7d69
Kiren <kirenp.2004@gmail.com>
2021-08-18 14:18:26 -0300

    Added template

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
+#add dummy data and make cards
+
+
 @app.route("/")
 def hello_world():
-    return render_template("layout.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
