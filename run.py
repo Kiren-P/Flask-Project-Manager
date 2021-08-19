13d9787a7c8083da4e1ccd7de0a47d603f9633d2
Kiren <kirenp.2004@gmail.com>
2021-08-19 14:04:18 -0300

    Added and displayed dummy data

--- run.py
+++ run.py
@@ -3,12 +3,24 @@ from flask import render_template
 
 app = Flask(__name__)
 
-#add dummy data and make cards
+#learn how to work with grrids in css
+projetcs = [
+    {"title":"test project",
+    "description":"This is the first project",
+    "manager":"Kiren Panchoe"
+    },
+    {"title":"test project 2",
+    "description":"This is the second project",
+    "manager":"Kiren Panchoe"
+    }
+
+    ]
+
 
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", projects=projetcs)
 
 if __name__ == "__main__":
     app.run(debug=True)
