65e915f4677161d0ec2c4040f00a3c5fa88e8029
Kiren <kirenp.2004@gmail.com>
2021-08-31 13:16:55 -0300

    Added dummy data to database and displayed using database

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
 
-#add data to database and display that data instead of dummy data
+#make create new project page
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String)
-    manager = db.Column(db.String, unique=False, nullable=False)
+    manager = db.Column(db.String, nullable=False)
     #tasks
 
     def __repr__(self):
@@ -22,59 +22,15 @@ class Project(db.Model):
 
 
 
-projetcs = [
-    {"title":"test project",
-    "description":"This is the first project",
-    "manager":"Kiren Panchoe",
-    "id":"0"
-    },
-    {"title":"test project 2",
-    "description":"This is the second project",
-    "manager":"Kiren Panchoe",
-    "id":"1"
-    },
-    {"title":"test project 3",
-    "description":"This is the thirdf project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    },
-    {"title":"test project 4",
-    "description":"This is the fourth project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    },
-    {"title":"test project 5",
-    "description":"This is the five project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    },
-    {"title":"test project 6",
-    "description":"This is the five project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    },
-    {"title":"test project 7",
-    "description":"This is the five project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    },
-    {"title":"test project 8",
-    "description":"This is the five project",
-    "manager":"Kiren Panchoe",
-    "id":"2"
-    }
-    ]
-
-
-
 @app.route("/")
 def homePage():
-    return render_template("home.html", projects=projetcs)
+    projects_all = Project.query.all()
+    return render_template("home.html", projects=projects_all)
 
-#when implementing databases use project title
-@app.route("/project/<project_id>")
-def projectPage(project_id):
-    project = projetcs[int(project_id)]
+@app.route("/project/<project_title>")
+def projectPage(project_title):
+    project = Project.query.filter_by(title=str(project_title)).first()
     return render_template("project.html", project=project)
+
 if __name__ == "__main__":
     app.run(debug=True)
